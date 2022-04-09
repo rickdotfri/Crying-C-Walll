9d7fa1471fd81454000352c5d7bd8ab72dff8a6c
rickdotfri <75217395+rickdotfri@users.noreply.github.com>
2022-04-09 19:19:26 -0400

    First but actually 2nd file

---
+++ CryingWall.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int  main() {
+char sad[] = "Thats Because my lover is a fucking teenager and i hate her i dont want to love her anymore and im crying because of that my only solution is finding a new girl but thats really hard amk";
+printf(" Why are you sad \n");
+printf ("%s \n",sad);
+printf("Thats all crying for me for this time bye 4 now \n");
+return 0;
+}
